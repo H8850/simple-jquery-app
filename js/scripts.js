aa97b215ce4d25260f82bb6bbb1d8bc293b3a867
H8850 <70569109+H8850@users.noreply.github.com>
2020-10-21 11:38:31 +0200

    changed colors

--- js/scripts.js
+++ js/scripts.js
@@ -2,38 +2,38 @@
 
 //JQuery code
 
-//1. Adding a new list item:
+function newItem() {
 
-let li = $('<li></li>');
-let inputValue = $('#input').val();
-li.append(inputValue);
+  //1. adding new list items
+  let li = $('<li></li>');
+  let inputValue = $('#input').val();
+  li.append(inputValue);
 
-if (inputValue === '') {
-  alert("You must write something!");
-} else {
-  $('#list').append(li);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    $('#list').append(li);
+  }
+
+  //2. Crossing out an item from the list
+
+  li.on("dblclick", function crossOut(){
+    li.toggleClass("strike");
+  });
+
+  //3. Adding delete button
+
+  let crossOutButton = $('<crossOutButton></crossOutButton>');
+  crossOutButton.append(document.createTextNode('X'));
+  li.append(crossOutButton);
+
+  crossOutButton.on("click", deleteListItem);
+  function deleteListItem() {
+    li.addClass("delete")
+  }
+
+  //4. adding sortable functionality
+
+  $('#list').sortable();
+
 }
-
-//2. Crossing out an item from the list
-
-function crossOut() {
-  li.toggleClass("strike");
-}
-
-li.on("dblclick", crossOut());
-
-//3. Adding delete button
-
-let crossOutButton = $('<crossOutButton></crossOutButton>');
-crossOutButton.append(document.createTextNode('X'));
-li.append(crossOutButton);
-
-function deleteListItem() {
-  li.addClass("delete")
-}
-
-crossOutButton.on("click", deleteListItem());
-
-//4. adding sortable functionality
-
-$('#list').sortable();
